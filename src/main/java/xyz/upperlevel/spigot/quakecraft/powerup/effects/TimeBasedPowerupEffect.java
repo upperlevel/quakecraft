de891c1277acc2db4bbdee75d154abf0d3c12ffb
Rossi Lorenzo <snowycoder@gmail.com>
2017-08-04 12:03:03 +0200

    Fix multi powerup cancelling out

--- src/main/java/xyz/upperlevel/spigot/quakecraft/powerup/effects/TimeBasedPowerupEffect.java
+++ src/main/java/xyz/upperlevel/spigot/quakecraft/powerup/effects/TimeBasedPowerupEffect.java
@@ -3,14 +3,20 @@ package xyz.upperlevel.spigot.quakecraft.powerup.effects;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 import xyz.upperlevel.spigot.quakecraft.QuakeCraftReloaded;
 import xyz.upperlevel.spigot.quakecraft.game.Participant;
 import xyz.upperlevel.uppercore.config.Config;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class TimeBasedPowerupEffect extends BasePowerupEffect {
     @Getter
     @Setter
     private int duration;
+    private Map<Player, BukkitTask> tasks = new HashMap<>();
 
     public TimeBasedPowerupEffect(String id) {
         super(id);
@@ -19,11 +25,17 @@ public abstract class TimeBasedPowerupEffect extends BasePowerupEffect {
     @Override
     public void apply(Participant participant) {
         start(participant);
-        Bukkit.getScheduler().runTaskLater(
+        final BukkitTask task = Bukkit.getScheduler().runTaskLater(
                 QuakeCraftReloaded.get(),
-                () -> end(participant),
+                () -> {
+                    tasks.remove(participant.getPlayer());
+                    end(participant);
+                },
                 duration
         );
+        BukkitTask old = tasks.put(participant.getPlayer(), task);
+        if(old != null)
+            old.cancel();
     }
 
     public abstract void start(Participant player);
