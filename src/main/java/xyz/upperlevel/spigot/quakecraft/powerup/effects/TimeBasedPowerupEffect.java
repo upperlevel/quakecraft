645aef44eff5f6209e45ae6ea5c8fa72d4ad7dfa
Rossi Lorenzo <snowycoder@gmail.com>
2017-08-04 14:42:36 +0200

    Add powerup enable-disable messages

--- src/main/java/xyz/upperlevel/spigot/quakecraft/powerup/effects/TimeBasedPowerupEffect.java
+++ src/main/java/xyz/upperlevel/spigot/quakecraft/powerup/effects/TimeBasedPowerupEffect.java
@@ -8,7 +8,10 @@ import org.bukkit.scheduler.BukkitTask;
 import xyz.upperlevel.spigot.quakecraft.QuakeCraftReloaded;
 import xyz.upperlevel.spigot.quakecraft.game.Participant;
 import xyz.upperlevel.uppercore.config.Config;
+import xyz.upperlevel.uppercore.message.Message;
+import xyz.upperlevel.uppercore.message.MessageManager;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +19,8 @@ public abstract class TimeBasedPowerupEffect extends BasePowerupEffect {
     @Getter
     @Setter
     private int duration;
+    private Message activeMessage;
+    private Message deactiveMessage;
     private Map<Player, BukkitTask> tasks = new HashMap<>();
 
     public TimeBasedPowerupEffect(String id) {
@@ -23,17 +28,20 @@ public abstract class TimeBasedPowerupEffect extends BasePowerupEffect {
     }
 
     @Override
-    public void apply(Participant participant) {
-        start(participant);
+    public void apply(Participant player) {
+        start(player);
+        Collection<Player> messageReceivers = player.getPhase().getGame().getPlayers();
+        activeMessage.broadcast(messageReceivers, "name", player.getName());
         final BukkitTask task = Bukkit.getScheduler().runTaskLater(
                 QuakeCraftReloaded.get(),
                 () -> {
-                    tasks.remove(participant.getPlayer());
-                    end(participant);
+                    tasks.remove(player.getPlayer());
+                    end(player);
+                    deactiveMessage.broadcast(messageReceivers, "name", player.getName());
                 },
                 duration
         );
-        BukkitTask old = tasks.put(participant.getPlayer(), task);
+        BukkitTask old = tasks.put(player.getPlayer(), task);
         if(old != null)
             old.cancel();
     }
@@ -46,5 +54,8 @@ public abstract class TimeBasedPowerupEffect extends BasePowerupEffect {
     public void load(Config config) {
         super.load(config);
         this.duration = config.getIntRequired("duration");
+        MessageManager messages = MessageManager.load(config.getConfigRequired("messages"));
+        activeMessage = messages.get("active");
+        deactiveMessage = messages.get("deactive");
     }
 }
