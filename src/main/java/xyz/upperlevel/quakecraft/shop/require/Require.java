067e0cb2221b57381b7dc84bdf94f558d0a7fd52
Rossi Lorenzo <snowycoder@gmail.com>
2018-10-05 19:35:43 +0200

    Remove old Require methods/fields

--- src/main/java/xyz/upperlevel/quakecraft/shop/require/Require.java
+++ src/main/java/xyz/upperlevel/quakecraft/shop/require/Require.java
@@ -4,17 +4,10 @@ import org.bukkit.ChatColor;
 import xyz.upperlevel.quakecraft.QuakeAccount;
 
 public interface Require {
-    String DONE = ChatColor.GREEN + "✔";
-    String MISSING = ChatColor.RED + "✖";
     ChatColor DESCRIPTION_BASE = ChatColor.DARK_GRAY;
     ChatColor DESCRIPTION_VALUE = ChatColor.DARK_AQUA;
     ChatColor PROGRESS_BASE = ChatColor.GRAY;
     ChatColor PROGRESS_VALUE = ChatColor.DARK_AQUA;
-
-
-    default String getRequires(QuakeAccount player) {
-        return "Requires \"" + getName(player) + "\" " + getType() + "!";
-    }
 
     String getName(QuakeAccount player);
 
